c96f174aa232bedd85a43c2361b1e111184c527c
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-03-13 15:05:46 -0500

    feat(tdd-app):chp1-4 Build Notes app in Django with TDD [ICTRAINING-1625]

---
+++ tddproject/notes/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from django.http import HttpRequest  
+from notes.views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
