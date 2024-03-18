de8c052e6abd2853a7b494ee5cfab3153534238f
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-03-17 21:38:42 -0500

    feat(add-app):chp5 Back it with db ICTRAINING-1626

--- tddproject/notes/tests.py
+++ tddproject/notes/tests.py
@@ -1,9 +1,48 @@
-from django.test import TestCase
-from django.http import HttpRequest  
 from notes.views import home_page
-
+from django.test import TestCase
+from django.http import HttpRequest
+from notes.models import Item  
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+
+    def test_uses_home_template(self):
+        [...]
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
         response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+
+    def test_can_save_a_POST_request(self):
+        self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
+
+    def test_redirects_after_POST(self):
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)    
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
--- tddproject/notes/views.py
+++ tddproject/notes/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from notes.models import Item
 # Create your views here.
 
 def home_page(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+
+    items = Item.objects.all()
+    return render(request, "home.html", {"items": items})
