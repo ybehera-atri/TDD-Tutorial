4d157ebe6dfefe9be8ff0534fee69d6291d9fe97
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-03-20 13:39:55 -0500

    feat(GitHub-actions):actions reviewer Reviewer script test run with get and post ICTRAINING-1633

--- tddproject/automation/reviewer.py
+++ tddproject/automation/reviewer.py
@@ -6,24 +6,54 @@ def check_update_reviewer(repo, pr, token):
 
     repository = repo
     pull_num = pr
-    base_api = f'https://api.github.com/repos/'
+    base_url = f'https://api.github.com/repos/'
     reviewer_api = f'{repository}/pulls/{pull_num}/requested_reviewers'
 
-    headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github+json"}
+    headers = {f"Authorization": f"token {token}", f"Accept": f"application/vnd.github+json"}
 
+    #mandatory reviewers
+    reviewer_list = ["bruschiusc"]
+
+
+    #check the current reviewers requested
     try:
 
-        reviewers_check = requests.get(base_api+reviewer_api, headers=headers).json()
+        reviewers_check = requests.get(base_url+reviewer_api, headers=headers).json()
         reviewers_requested = reviewers_check['users']
         print(reviewers_requested)
-        print(type(reviewers_requested))
 
     except Exception as e:
 
         print(f'Exception occurred with error {e}')    
     
     
-    
+    #if mandatory reviewer not present request
+    try:
+
+
+        list_new = [i for i in reviewer_list if i not in reviewers_requested]
+
+        #payload
+        reviewers = {
+        f"reviewers" : list_new
+        }
+
+        reviewer_request = requests.post(base_url+reviewer_api, headers=headers, data = json.dumps(reviewers))
+
+        print(reviewer_request.content)
+
+    except Exception as e:
+
+        print(f'Exception occurred with erro {e}')    
+
+
+
+
     
 
+
+
+
+
+#call
 check_update_reviewer(os.getenv('GITHUB_REPOSITORY'),os.getenv('PULL_NUMBER'),os.getenv('GITHUB_TOKEN'))
