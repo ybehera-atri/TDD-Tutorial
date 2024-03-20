81fbb0229779595d3e0d3aa088069027cba450e1
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-03-20 14:15:05 -0500

    feat(GitHub-actions):actions reviewer Reviewer script test run with get and post refactored ICTRAINING-1633

--- tddproject/automation/reviewer.py
+++ tddproject/automation/reviewer.py
@@ -2,8 +2,8 @@ import requests
 import os
 import json
 
-def check_update_reviewer(repo, pr, token):
 
+def check_update_reviewer(repo, pr, token):
     repository = repo
     pull_num = pr
     base_url = f'https://api.github.com/repos/'
@@ -11,49 +11,39 @@ def check_update_reviewer(repo, pr, token):
 
     headers = {f"Authorization": f"token {token}", f"Accept": f"application/vnd.github+json"}
 
-    #mandatory reviewers
+    # mandatory reviewers
     reviewer_list = ["bruschiusc"]
 
-
-    #check the current reviewers requested
+    # check the current reviewers requested
     try:
 
-        reviewers_check = requests.get(base_url+reviewer_api, headers=headers).json()
+        reviewers_check = requests.get(base_url + reviewer_api, headers=headers).json()
         reviewers_requested = reviewers_check['users']
         print(reviewers_requested)
 
     except Exception as e:
 
-        print(f'Exception occurred with error {e}')    
-    
-    
-    #if mandatory reviewer not present request
-    try:
+        print(f'Exception occurred with error {e}')
 
+        # if mandatory reviewer not present request
+    try:
 
         list_new = [i for i in reviewer_list if i not in reviewers_requested]
 
-        #payload
+        # payload
         reviewers = {
-        f"reviewers" : list_new
+            f"reviewers": list_new
         }
 
-        reviewer_request = requests.post(base_url+reviewer_api, headers=headers, data = json.dumps(reviewers))
+        reviewer_request = requests.post(base_url + reviewer_api, headers=headers, data=json.dumps(reviewers))
 
         print(reviewer_request.content)
 
     except Exception as e:
 
-        print(f'Exception occurred with erro {e}')    
-
-
-
+        print(f'Exception occurred with erro {e}')
 
     
 
-
-
-
-
-#call
-check_update_reviewer(os.getenv('GITHUB_REPOSITORY'),os.getenv('PULL_NUMBER'),os.getenv('GITHUB_TOKEN'))
+# call
+check_update_reviewer(os.getenv('GITHUB_REPOSITORY'), os.getenv('PULL_NUMBER'), os.getenv('GITHUB_TOKEN'))
