e172cb25a7ff3ce6af22bf75b3c6eeddc68686ce
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-03-20 11:41:20 -0500

    feat(GitHub-actions):actions reviewer Reviewer script to get reviewers ICTRAINING-1633

--- tddproject/automation/reviewer.py
+++ tddproject/automation/reviewer.py
@@ -2,9 +2,19 @@ import requests
 import os
 import json
 
-def check_reviewer(repo, pr):
+def check_update_reviewer(repo, pr, token):
 
-    print(repo)
-    print(pr)
+    repository = repo
+    pull_num = pr
+    base_api = f'https://api.github.com/repos/'
+    reviewer_api = f'{repository}/pulls/{pull_num}/requested_reviewers'
 
-check_reviewer(os.getenv('GITHUB_REPOSITORY'),os.getenv('PULL_NUMBER'))
+    headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github+json"}
+
+    reviewers_check = requests.get(base_api+reviewer_api, headers=headers)
+    
+    print(reviewers_check)
+    
+    
+
+check_update_reviewer(os.getenv('GITHUB_REPOSITORY'),os.getenv('PULL_NUMBER'),os.getenv('GITHUB_TOKEN'))
