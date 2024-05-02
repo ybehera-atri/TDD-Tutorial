5dd5a2d97ca87db0a92baabbc6911844f78f4e82
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-05-02 17:46:16 -0500

    feat(ReviewerAutomation) [EDC-3211] debug

--- tddproject/automation/test_reviewer.py
+++ tddproject/automation/test_reviewer.py
@@ -47,7 +47,7 @@ def check_update_reviewer(repo, pr, token):
         if branch != 'main_django_3_2' or 'main_django_3_2_deployment':
             committers_info = requests.get(
                 base_url + committer_api, headers=headers).json()
-            for values in committers_info['committer']:
+            for values in committers_info:
                 print(f'{values}')
 
             if committers_info.status_code == 200:
