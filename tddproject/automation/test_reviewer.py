acc6324b7743d23b2db862b739df5df03254c231
Yashraj Behera <ybehera@ATRI003265.cable.rcn.com>
2024-05-03 15:06:17 -0500

    feat(ReviewerAutomation) [EDC-3211] debug

--- tddproject/automation/test_reviewer.py
+++ tddproject/automation/test_reviewer.py
@@ -52,8 +52,7 @@ def check_update_reviewer(repo, pr, token):
             data_json = json.loads(committer_text)
             #print(f'{type(data_json)} {data_json}')
             for names in data_json:
-                if 'committer' in names:
-                    print(names)
+                print(names.get('commit'))
 
             if committers_info.status_code == 200:
                 print(type(data_json))
